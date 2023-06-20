904cba9f0b11413ecaf1e87ebd81df27486b7053
MR-NC <61655493+MR-NC@users.noreply.github.com>
2023-06-20 14:00:26 +0700

    Delete system' directory

--- system'/commands/runtime.js
+++
@@ -1,9 +0,0 @@
-const { runtime } = require("@libs/function")
-module.exports = {
-    commands: ["runtime"],
-    cooldown: 13,
-    isSewa: true,
-    callback: async ({ m }) => {
-        m.reply(`${runtime(process.uptime())}`)
-    }
-}
--- system'/commands/speed.js
+++
@@ -1,9 +0,0 @@
-const moment = require("moment-timezone")
-module.exports = {
-    commands: ["speed"],
-    cooldown: 13,
-    isSewa: true,
-    callback: async ({ m }) => {
-        m.reply(`*_${moment.duration(Date.now() - parseInt(m.messageTimestamp.toString()) * 1000).asSeconds()} second_*`)
-    }
-}
